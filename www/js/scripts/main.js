8ed16a2489c18e896ac4b275f0c4cf42b9669762
CrazieSiberian <32862613+BenteTheArchitect@users.noreply.github.com>
2019-05-09 16:56:16 -0500

    Initial file upload

---
+++ www/js/scripts/main.js
@@ -0,0 +1,15 @@
+// Setup dependencies
+requirejs.config({
+  baseUrl: "js",
+  paths: {
+    "jquery": ["https://code.jquery.com/jquery-2.1.4.min", "lib/jquery-2.1.4"],
+    "jquery-mobile": ["https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min", "lib/jquery-mobile-1.4.5"],
+  },
+});
+
+// App logic
+requirejs(["jquery", "jquery-mobile"], function() {
+  $(document).ready(function() {
+    console.log("DOM Ready");
+  });
+});
